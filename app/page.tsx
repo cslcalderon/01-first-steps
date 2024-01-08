ee7f4de07716f221d6428feab0f2eaa776e7a04f
sofia calderon <cslcalderon@gmail.com>
2024-01-08 11:31:33 -0600

    adding pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <span className = "text-5xl">Hello World</span>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <span className = "text-5xl">
+        Hello World 
+        { new Date().getTime()}
+      </span>
     </main>
   )
 }
